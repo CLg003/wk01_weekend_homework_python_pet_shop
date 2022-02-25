7bc0a1b8c5638c750dfafc96d63b5788170d3753
CLg003 <clairemjlaing@gmail.com>
2022-02-25 17:33:04 +0000

    customer_can_afford_pet, sell_pet_to_customer (2/3)

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -56,5 +56,140 @@ def get_customer_pet_count(customer):
 def add_pet_to_customer(customer, pet):
     customer["pets"].append(pet)
 
+def customer_can_afford_pet(customer, pet):
+    if customer["cash"] >= pet["price"]:
+        return True
+    return False
+
+customers = [
+            {
+                "name": "Alice",
+                "pets": [],
+                "cash": 1000
+            },
+            {
+                "name": "Bob",
+                "pets": [],
+                "cash": 50
+            },
+            {
+                "name": "Jack",
+                "pets": [],
+                "cash": 100
+            }
+        ]
+
+new_pet = {
+    "name": "Bors the Younger",
+    "pet_type": "cat",
+    "breed": "Cornish Rex",
+    "price": 100
+}
+
+cc_pet_shop = {
+    "pets": [
+        {
+            "name": "Sir Percy",
+            "pet_type": "cat",
+            "breed": "British Shorthair",
+            "price": 500
+        },
+        {
+            "name": "King Bagdemagus",
+            "pet_type": "cat",
+            "breed": "British Shorthair",
+            "price": 500
+        },
+        {
+            "name": "Sir Lancelot",
+            "pet_type": "dog",
+            "breed": "Pomsky",
+            "price": 1000,
+        },
+        {
+            "name": "Arthur",
+            "pet_type": "dog",
+            "breed": "Husky",
+            "price": 900,
+        },
+        {
+            "name": "Tristan",
+            "pet_type": "cat",
+            "breed": "Basset Hound",
+            "price": 800,
+        },
+        {
+            "name": "Merlin",
+            "pet_type": "cat",
+            "breed": "Egyptian Mau",
+            "price": 1500,
+        }
+    ],
+    "admin": {
+        "total_cash": 1000,
+        "pets_sold": 0,
+    },
+    "name": "Camelot of Pets"
+}
+
+# def find_pet_by_name(pet_shop, name):
+#     for pet in pet_shop["pets"]:
+#         if pet["name"] == name:
+#             return pet
+#     return None
+
+def sell_pet_to_customer(pet_shop, pet, customer):
+    if (find_pet_by_name(pet_shop, pet["name"])) == None:
+        pass
+    if (customer_can_afford_pet(customer, pet) == True):
+        remove_customer_cash(customer, pet["price"])
+        add_or_remove_cash(pet_shop, pet["price"]) # +/-price??
+        add_pet_to_customer(customer, pet)
+        remove_pet_by_name(pet_shop, pet["name"])
+        increase_pets_sold(pet_shop, 1)
+    
+sell_pet_to_customer(cc_pet_shop, cc_pet_shop["pets"][3], customers[0])
+print(get_customer_pet_count(customers[0]))
+print(get_pets_sold(cc_pet_shop))
+print(get_customer_cash(customers[0]))
+print(get_total_cash(cc_pet_shop))
 
 
+# These are 'integration' tests so we want multiple asserts.
+# If one fails the entire test should fail
+#
+# @unittest.skip("delete this line to run the test")
+def test_sell_pet_to_customer__pet_found(self):
+    customer = self.customers[0]
+    pet = find_pet_by_name(self.cc_pet_shop,"Arthur")
+
+    sell_pet_to_customer(self.cc_pet_shop, pet, customer)
+
+    self.assertEqual(1, get_customer_pet_count(customer))
+    self.assertEqual(1, get_pets_sold(self.cc_pet_shop))
+    self.assertEqual(100, get_customer_cash(customer))
+    self.assertEqual(1900, get_total_cash(self.cc_pet_shop))
+    
+# @unittest.skip("delete this line to run the test")
+def test_sell_pet_to_customer__pet_not_found(self):
+    customer = self.customers[0]
+    pet = find_pet_by_name(self.cc_pet_shop,"Dave")
+
+    sell_pet_to_customer(self.cc_pet_shop, pet, customer)
+
+    self.assertEqual(0, get_customer_pet_count(customer))
+    self.assertEqual(0, get_pets_sold(self.cc_pet_shop))
+    self.assertEqual(1000, get_customer_cash(customer))
+    self.assertEqual(1000, get_total_cash(self.cc_pet_shop))
+
+# @unittest.skip("delete this line to run the test")
+def test_sell_pet_to_customer__insufficient_funds(self):
+    customer = self.customers[1]
+    pet = find_pet_by_name(self.cc_pet_shop,"Arthur")
+
+    sell_pet_to_customer(self.cc_pet_shop, pet, customer)
+
+    self.assertEqual(0, get_customer_pet_count(customer))
+    self.assertEqual(0, get_pets_sold(self.cc_pet_shop))
+    self.assertEqual(50, get_customer_cash(customer))
+    self.assertEqual(1000, get_total_cash(self.cc_pet_shop))
