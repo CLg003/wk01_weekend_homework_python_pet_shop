3e9cc6c89f11d4f9c07d35b10cc39d9f7e96b0a4
CLg003 <clairemjlaing@gmail.com>
2022-02-25 15:36:34 +0000

    get_customer_cash, remove_customer_cash, get_customer_pet_count, add_pet_to_customer

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -44,3 +44,17 @@ def remove_pet_by_name(pet_shop, name):
 def add_pet_to_stock(pet_shop, new_pet):
     pet_shop["pets"].append(new_pet)
 
+def get_customer_cash(customer):
+    return customer["cash"]
+
+def remove_customer_cash(customer, amount):
+    customer["cash"] -= amount
+
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
+
+def add_pet_to_customer(customer, pet):
+    customer["pets"].append(pet)
+
+
+
