322d060f0e9edd0676b2d7136faeec913fd63697
CLg003 <clairemjlaing@gmail.com>
2022-02-25 15:27:20 +0000

    get_pets_by_breed, find_pet_by_name, remove_pet_by_name, add_pet_to_stock

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -20,3 +20,27 @@ def increase_pets_sold(pet_shop, new_pets_sold):
 def get_stock_count(pet_shop):
     return len(pet_shop["pets"])
 
+def get_pets_by_breed(pet_shop, breed):
+    pets_by_breed = []
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == breed:
+            pets_by_breed.append(pet)
+    return pets_by_breed
+
+def find_pet_by_name(pet_shop, name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == name:
+            return pet
+    return None
+
+# def remove_pet_by_name(pet_shop, name):
+#     for pet in pet_shop["pets"]:
+#         if pet["name"] == name:
+#             pet_shop["pets"].remove(pet)
+
+def remove_pet_by_name(pet_shop, name):
+    pet_shop["pets"].remove(find_pet_by_name(pet_shop, name))
+
+def add_pet_to_stock(pet_shop, new_pet):
+    pet_shop["pets"].append(new_pet)
+
